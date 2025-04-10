d39d411c5d5b09ff46558fef389aa0547caa8b89
siam1024 <siam35-1024@diu.edu.bd>
2025-04-10 14:26:01 +0600

    update dashboard page mistake

--- app/(dashboardLayout)/dashboard/page.tsx
+++ app/(dashboardLayout)/dashboard/page.tsx
@@ -1,29 +1,11 @@
-"use client";
+import React from 'react';
 
-import StoreProvider from "@/app/StoreProvide";
-import Sidebar from "@/components/modules/SideBar";
-import Footer from "@/components/shared/Footer";
-import NavBar from "@/components/shared/NavBar";
-import React from "react";
-
-export default function DashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const page = () => {
   return (
-    <StoreProvider>
-      <NavBar />
-      <div className="flex h-screen overflow-hidden">
-        {/* Sidebar */}
-        <Sidebar />
-
-        {/* Main Content */}
-        <div className="flex flex-col flex-1 overflow-y-auto">
-          <main className="flex-1 p-4">{children}</main>
-        </div>
-      </div>
-      <Footer />
-    </StoreProvider>
+    <div>
+      
+    </div>
   );
-}
+};
+
+export default page;
--- app/(dashboardLayout)/layout.tsx
+++ app/(dashboardLayout)/layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import Sidebar from "@/components/modules/SideBar";
 import Footer from "@/components/shared/Footer";
 import NavBar from "@/components/shared/NavBar";
 import { useAuth } from "@/hooks/useAuth";
@@ -10,18 +12,23 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isAuthenticated = useAuth();
-
-  if (!isAuthenticated) return null; // Prevent rendering before checking auth
-
+  const auth = useAuth();
+  if (!auth) {
+    return null;
+  }
   return (
-    <>
-      <StoreProvider>
-        {" "}
-        <NavBar />
-        {children}
-        <Footer />
-      </StoreProvider>
-    </>
+    <StoreProvider>
+      <NavBar />
+      <div className="flex h-screen overflow-hidden">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <div className="flex flex-col flex-1 overflow-y-auto">
+          <main className="flex-1 p-4">{children}</main>
+        </div>
+      </div>
+      <Footer />
+    </StoreProvider>
   );
 }
