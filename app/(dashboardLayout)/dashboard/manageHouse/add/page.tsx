6b2387c016ac989cbc8022940ac55c9580ca4bae
Ebrahim683 <ebrahimrahman20@gmail.com>
2025-04-10 07:36:34 +0600

    NIK-86 create house

---
+++ app/(dashboardLayout)/dashboard/manageHouse/add/page.tsx
@@ -0,0 +1,13 @@
+import AddHouseForm from "@/components/Forms/AddHouseForm";
+
+const AddHousePage = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="my-10 ">
+        <AddHouseForm />
+      </div>
+    </div>
+  );
+};
+
+export default AddHousePage;
